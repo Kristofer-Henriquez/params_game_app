7fe3d284705ba45c32505c7dcab58f713bbaa12a
Kristofer <kristoferhenriquez@gmail.com>
2020-07-02 17:33:53 -0500

    impemented guess game using route+controller+json file

--- app/controllers/api/param_examples_controller.rb
+++ app/controllers/api/param_examples_controller.rb
@@ -1,2 +1,18 @@
 class Api::ParamExamplesController < ApplicationController
+
+  def name
+    render "query_name.json.jb"
+  end
+
+  def guess_game
+    guess_answer = params['guess'].to_i
+    if guess_answer > 32
+      @message = "you guessed too high"
+    elsif guess_answer < 32
+      @message = "you guessed too low"
+    else
+      @message = "That's correct!"
+    end
+    render "guess_game.json.jb"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,10 @@ Rails.application.routes.draw do
   # get "/photos" => "photos#index"
 
   # EXAMPLE JSON ROUTE WITH API NAMESPACE
-  # namespace :api do
-  #   get "/photos" => "photos#index"
-  # end
+
+  namespace :api do
+    get "/paramsgamesurl" => "param_examples#name"
+
+    get "/paramsgamesurl/:guess" => "param_examples#guess_game"
+  end
 end
